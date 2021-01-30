2bd1c348fd11659edce582a444f33768a60692ab
Hajoo Chung <hajoo.chung@gmail.com>
2021-01-30 00:01:30 +0000

    prepare deploy

--- src/components/AuthForm.js
+++ src/components/AuthForm.js
@@ -1,8 +1,6 @@
 import { authService } from "myFirebase";
 import React, { useState } from "react";
 
-// const inputStyles = {};
-
 const AuthForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
